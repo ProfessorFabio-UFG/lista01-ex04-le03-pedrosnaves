aed54a5ddf4eda366ea7ed9249bf0400f59eb904
pedrosnaves <pedrosnaves@gmail.com>
2025-04-04 21:50:55 -0300

    Update Teste.java

--- ex04/TesteRobo.java
+++ ex04/TesteRobo.java
@@ -1,27 +1,12 @@
+
 import java.util.Scanner;
 
-public class TesteRobo {
+public class Teste {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Robo 01");
-        System.out.println("ID: ");
-        int id1 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Limite de po: ");
-        int lmt_po1 = sc.nextInt();
-        sc.nextLine();
-        Robo robo1 = new Robo(id1, lmt_po1);
-
-        System.out.println("Robo 02");
-        System.out.println("ID: ");
-        int id2 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Limite de po: ");
-        int lmt_po2 = sc.nextInt();
-        sc.nextLine();
-        Robo robo2 = new Robo(id2, lmt_po2);
-
+        Robo robo1 = new Robo(1111, 1000);
+        Robo robo2 = new Robo(2222, 2000);
 
         robo1.ligar();
         robo1.andar(10, 25);
@@ -43,6 +28,5 @@ public class TesteRobo {
 
         System.out.println(robo1);
         System.out.println(robo2);
-
     }
 }
